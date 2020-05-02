1e92a8642648061278b94e76b3f8bd2ec0ea1f61
= <=>
2020-05-02 22:59:44 +0530

    feat: initial bootstrap of project using operator sdk framework

---
+++ pkg/apis/ravishankarsr/group.go
@@ -0,0 +1,6 @@
+// Package ravishankarsr contains ravishankarsr API versions.
+//
+// This file ensures Go source parsers acknowledge the ravishankarsr package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package ravishankarsr
